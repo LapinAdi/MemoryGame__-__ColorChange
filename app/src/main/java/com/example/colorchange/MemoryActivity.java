08aed71d8cf4ade8e59fb86e90292a50f3c53591
Adi <ofe2.adilap@solisrael.co.il>
2022-09-11 14:06:53 +0300

    finished colorChange project
    working on memory game project
    - switch between screen -added back button- working
    - last touch up on design

--- app/src/main/java/com/example/colorchange/MemoryActivity.java
+++ app/src/main/java/com/example/colorchange/MemoryActivity.java
@@ -4,25 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MemoryActivity extends AppCompatActivity {
+public class MemoryActivity extends AppCompatActivity implements View.OnClickListener{
 
+    // שלב א הגדרת רכיבים
+    private String name1,
+                    name2;
+
+    private TextView tvName1,tvName2;
+
+    private Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory);
 
+        // שלב ב קישור רכיבים
+        tvName1= (TextView) findViewById(R.id.tvName1);
+        tvName2= (TextView) findViewById(R.id.tvName2);
+
+        btnBack =(Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(this);
         Intent in=getIntent();
         if(in!=null)
         {
             Bundle xtras=in.getExtras();
-            System.out.println(xtras.getString("DATA1"));
-            System.out.println(xtras.getString("DATA2"));
+
+            name1=xtras.getString("DATA1");
+            name2=xtras.getString("DATA2");
+
         }
+        tvName1.setText(name1);
+        tvName2.setText(name2);
 
 
 
 
+    }
+
+    public void onClick(View v)
+    {
+        if(v.getId()==btnBack.getId())
+        {
+            Intent intent=new Intent(this, AddPlayer.class);
+
+
+
+            startActivity(intent);
+        }
     }
 
 
